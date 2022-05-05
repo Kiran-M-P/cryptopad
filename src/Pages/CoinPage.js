5e5886c166dfa5035297dd05e743d79de3f331c5
Kiran M P <itskiranmp@gmail.com>
2022-05-05 22:06:34 +0530

    Coinpage sidebar done

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,4 +1,9 @@
-import { makeStyles, Typography, useTheme } from "@material-ui/core";
+import {
+  makeStyles,
+  Typography,
+  useTheme,
+  LinearProgress,
+} from "@material-ui/core";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -6,7 +11,7 @@ import CoinInfo from "../Components/CoinInfo";
 import { SingleCoin } from "../config/api";
 import { CryptoState } from "../CryptoContext";
 import parse from "html-react-parser";
-
+import { numberWithCommas } from "../Components/Banner/Carousel";
 const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
@@ -31,6 +36,31 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: 20,
     fontFamily: "Jetbrains Mono",
   },
+  description: {
+    width: "100%",
+    fontFamily: "Jetbrains Mono",
+    padding: 25,
+    paddingBottom: 15,
+    paddingTop: 0,
+    textAlign: "justify",
+  },
+  marketData: {
+    alignSelf: "start",
+    padding: 25,
+    paddingTop: 10,
+    width: "100%",
+    [theme.breakpoints.down("md")]: {
+      display: "flex",
+      justifyContent: "space-around",
+    },
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    [theme.breakpoints.down("smxs")]: {
+      alignItems: "start",
+    },
+  },
 }));
 
 const CoinPage = () => {
@@ -40,7 +70,6 @@ const CoinPage = () => {
 
   const theme = useTheme();
   const classes = useStyles();
-
   useEffect(() => {
     const fetchCoin = async () => {
       const { data } = await axios.get(SingleCoin(id));
@@ -51,6 +80,7 @@ const CoinPage = () => {
 
   console.log(coin);
 
+  if (!coin) return <LinearProgress />;
   return (
     <div className={classes.container}>
       <div className={classes.sidebar}>
@@ -68,6 +98,72 @@ const CoinPage = () => {
         <Typography variant="subtitle1" className={classes.description}>
           {parse(String(coin?.description.en.split(". ")[0]))}.
         </Typography>
+        <div className={classes.marketData}>
+          <span
+            style={{
+              display: "flex",
+            }}
+          >
+            <Typography variant="h5" className={classes.heading}>
+              Rank:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Jetbrains Mono",
+              }}
+            >
+              {coin?.market_cap_rank}
+            </Typography>
+          </span>
+
+          <span
+            style={{
+              display: "flex",
+            }}
+          >
+            <Typography variant="h5" className={classes.heading}>
+              Current Price:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Jetbrains Mono",
+              }}
+            >
+              {symbol}
+              {numberWithCommas(
+                coin?.market_data.current_price[currency.toLowerCase()]
+              )}
+            </Typography>
+          </span>
+          <span
+            style={{
+              display: "flex",
+            }}
+          >
+            <Typography variant="h5" className={classes.heading}>
+              Market Cap:
+            </Typography>
+            &nbsp; &nbsp;
+            <Typography
+              variant="h5"
+              style={{
+                fontFamily: "Jetbrains Mono",
+              }}
+            >
+              {symbol}
+              {numberWithCommas(
+                coin?.market_data.market_cap[currency.toLowerCase()]
+                  .toString()
+                  .slice(0, -6)
+              )}
+              M
+            </Typography>
+          </span>
+        </div>
       </div>
       <CoinInfo coin={coin} />
     </div>

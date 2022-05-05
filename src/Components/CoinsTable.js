27854d478e211db6f65091238acca7b04a497e52
Kiran M P <itskiranmp@gmail.com>
2022-05-05 13:17:48 +0530

    debugged handleSearch

--- src/Components/CoinsTable.js
+++ src/Components/CoinsTable.js
@@ -1,20 +1,37 @@
 import {
   Container,
   createTheme,
+  LinearProgress,
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
   ThemeProvider,
 } from "@material-ui/core";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { CoinList } from "../config/api";
 import { CryptoState } from "../CryptoContext";
 
+// styles
+const useStyles = makeStyles(() => ({
+  row: {},
+}));
+
 const CoinsTable = () => {
+  // states
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
-
   const { currency, symbol } = CryptoState();
 
+  const [search, setSearch] = useState("");
+
+  // coins data fetch
   useEffect(() => {
     const fetchCoins = async () => {
       setLoading(true);
@@ -25,6 +42,7 @@ const CoinsTable = () => {
     fetchCoins();
   }, [currency]);
 
+  // theme
   const darkTheme = createTheme({
     palette: {
       primary: {
@@ -34,11 +52,87 @@ const CoinsTable = () => {
     },
   });
 
-  console.log(coins);
+  // search
+  const handleSearch = () => {
+    return coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(search) ||
+        coin.symbol.toLowerCase().includes(search)
+    );
+  };
+
+  // navigate
+  const navigate = useNavigate();
+
+  const classes = useStyles();
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Container style={{ textAlign: "center" }}>
-        <TextField label="Search" variant="outlined"></TextField>
+        <TextField
+          onChange={(e) => setSearch(e.target.value)}
+          label="Search"
+          variant="outlined"
+          style={{
+            marginTop: 20,
+            marginBottom: 20,
+            width: "100%",
+          }}
+        ></TextField>
+        <TableContainer>
+          {loading ? (
+            <LinearProgress />
+          ) : (
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                    <TableCell
+                      key={head}
+                      style={{
+                        fontFamily: "Jetbrains Mono",
+                        fontWeight: 700,
+                      }}
+                      align={head === "Coin" ? "left" : "right"}
+                    >
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {handleSearch().map((row) => {
+                  const profit = row.price_change_percentage_24h > 0;
+                  return (
+                    <TableRow
+                      onClick={() => navigate.push(`/coins/${row.id}`)}
+                      className={classes.row}
+                      key={row.name}
+                    >
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        styles={{
+                          display: "flex",
+                          gap: 15,
+                        }}
+                      >
+                        <img
+                          src={row?.image}
+                          alt={row.name}
+                          height="50"
+                          style={{
+                            marginBottom: 10,
+                          }}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          )}
+        </TableContainer>
       </Container>
     </ThemeProvider>
   );
